b51aaa31f1908fb784456ddeb4381cc50255236b
Cas0n-McVey <absolgamer25@gmail.com>
2025-02-26 13:17:14 -0600

    C# Test

---
+++ HelloWorld_McVey_Cason/Program.cs
@@ -0,0 +1,26 @@
+﻿ // System namespace need to use Console class.
+  using System;
+ // My collection of classes for this project.
+namespace HelloWorld_Cason_McVey
+{
+
+    // Main class for this program
+    class Program
+    {
+        // The Main method is executed when application starts
+        static void Main(string[] args)
+        {
+            // Asks the user for their first name
+            Console.WriteLine("What is your first name?");
+            // Assigns user input to firstName variable
+            string firstName = Console.ReadLine();
+            // Asks the user for their last name
+            Console.WriteLine("What is your last name?");
+            // Assigns user input to lastName variable
+            string lastName = Console.ReadLine();
+
+            // Prints a welcome message with firstName and lastName
+            Console.WriteLine($"Hello, {firstName} {lastName}!");
+        }
+    }
+}
